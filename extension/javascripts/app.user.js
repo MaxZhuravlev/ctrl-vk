37be6328ba96882e18c7404e0918c98e67601063
Roman Milushov <github@milushov.ru>
2013-02-13 22:58:13 +0400

    fix condition; vanilla js click; fix indentation

--- extension/javascripts/app.user.js
+++ extension/javascripts/app.user.js
@@ -1,47 +1,46 @@
+console.log('app start');
 
-    console.log('app start');
+document.onpaste = function(event){
+  var items = event.clipboardData.items;
 
+  console.log(JSON.stringify(items)); // will give you the mime types
 
-    document.onpaste = function(event){
-        var items = event.clipboardData.items;
+  if ( item = items[1] ) {
+    if ( /^image/.test(item["type"]) ) {
+      console.log("image/png");
 
-        console.log(JSON.stringify(items)); // will give you the mime types
+      var blob = item.getAsFile();
+      var reader = new FileReader();
 
-        if(items[0]["type"]=="image/png"){
-            console.log("image/png");
+      /*
+        *
+      // например можно вставить картинку в страницу
+      reader.onload = function(event){
+      console.log(event.target.result); // data url!
+      var img = document.createElement('img')
+      img.src=event.target.result;
+      document.body.appendChild(img);
 
-            var blob = items[0].getAsFile();
-            var reader = new FileReader();
-
-            /*
-             // например можно вставить картинку в страницу
-             reader.onload = function(event){
-             console.log(event.target.result); // data url!
-             var img = document.createElement('img')
-             img.src=event.target.result;
-             document.body.appendChild(img);
-
-             };
-             reader.readAsDataURL(blob);
-             */
-
-
-            // If you want to upload it instead, you could use readAsBinaryString, or you could probably put it into an XHR using FormData https://developer.mozilla.org/en/XMLHttpRequest/FormData
-            reader.onload = function(event){
-
-                console.log(event.target.result); // data url!
-                var binaryString=event.target.result;
-
-                // а теперь заморочка со вставкой :) я даже кликнуть не могу
-                var button=$(".add_media_type_2_photo");
-                console.log(button);
-                button.click();
-                console.log("add_media_type_2_photo click");
-
-            };
-            reader.readAsBinaryString(blob);
-
-        }
+      };
+      reader.readAsDataURL(blob);
+      */
 
 
+      // If you want to upload it instead, you could use readAsBinaryString, or you could probably put it into an XHR using FormData https://developer.mozilla.org/en/XMLHttpRequest/FormData
+      reader.onload = function(event){
+
+        console.log(event.target.result); // data url!
+        var binaryString=event.target.result;
+
+        var button = document.getElementsByClassName('add_media_type_2_photo')[0]
+        console.log(button);
+
+        button.click();
+        console.log("add_media_type_2_photo click");
+
+      };
+
+      reader.readAsBinaryString(blob);
     }
+  }
+}
