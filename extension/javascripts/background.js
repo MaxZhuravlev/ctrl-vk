aae71bb15766a6c202201f2dc2f20cef14710fd1
Roma Milushov <roma@milushov.ru>
2013-02-16 20:58:28 +0400

    fix bug

--- extension/javascripts/background.js
+++ extension/javascripts/background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (app.getSettings 'authorizeInProgress') {
+  if (app.getSettings('authorizeInProgress')) {
     alert('my tab');
-    REDIRECT_URI = app.getSettings 'REDIRECT_URI'
+    REDIRECT_URI = app.getSettings('REDIRECT_URI');
     if (tab.url.indexOf(REDIRECT_URI + "#access_token") >= 0) {
       app.setSettings('authorize_in_progress', false);
       chrome.tabs.remove(tabId);
